52c6741fe72eaed13fba3dd62a909061f212e0d6
Michael Salvatore <msalvatore22@gmail.com>
2023-01-31 15:36:58 -0500

    First commit

---
+++ src/main/java/com/msalvatore22/SpringMongoReact/SpringMongoReactApplication.java
@@ -0,0 +1,13 @@
+package com.msalvatore22.SpringMongoReact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMongoReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMongoReactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/msalvatore22/SpringMongoReact/SpringMongoReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msalvatore22.SpringMongoReact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMongoReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
